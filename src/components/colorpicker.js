50d2489ee28adb55d92e29b57e48f7e9dd2ad843
Caitlin Wang <caitlinwang97@gmail.com>
2021-03-06 20:48:25 -0500

    Adjust color picker size

--- src/components/colorpicker.js
+++ src/components/colorpicker.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { SketchPicker } from "react-color";
+import { ChromePicker, SketchPicker } from "react-color";
 
 const ColorPicker = ({ color, setColor }) => (
-  <SketchPicker
-    color={color}
-    onChangeComplete={(color) => {
-      setColor(color.hex);
-    }}
-    presetColors={['#4D31BF', '#14C0B6', '#B1DD52']}
-  />
+  <div className="bottom-screen">
+    <SketchPicker
+      color={color}
+      onChangeComplete={(color) => {
+        setColor(color.hex);
+      }}
+      presetColors={["#4D31BF", "#14C0B6", "#B1DD52"]}
+    />
+  </div>
 );
 
 export default ColorPicker;
