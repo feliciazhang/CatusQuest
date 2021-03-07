b6e7a423f36210937e3b9f06890958d1ea628430
glennbillman <billman.g@northeastern.edu>
2021-03-07 03:39:37 -0500

    editing scenes

--- src/data/scenes.js
+++ src/data/scenes.js
@@ -3,12 +3,12 @@ const catusRoom = {
     {
       text:
         "> You are asleep in bed until a faint sks sks sks sound wakes you up. Wait a damn minute…",
-      img: "img/catus-room.png",
+      img: "img/catu1.png",
     },
     {
       text:
         "> Eek! You open your eyes to see Ymer on your curtain. He’s scratching at something…\n> A milky white light starts to fill the room.",
-      img: "img/example.png",
+      img: "img/catu2.png",
     },
   ],
   question: "What do you do?",
@@ -18,17 +18,17 @@ const catusRoom = {
       leadsTo: null,
       message:
         "Uh oh! The white light burns through you and the rest of the known universe. Try again.",
-      audio: "audio/example.mp3",
+      audio: "audio/agh.mp3",
     },
     {
       text: "Investigate",
       leadsTo: "zachsCave",
       message: "Brave choice! #girlboss",
-      audio: "",
+      audio: "audio/come-this-way.mp3",
     },
   ],
-  audio: "audio/blueside.mp3",
-  img: "img/example.png",
+  audio: "audio/catusroom.mp3",
+  img: "img/catu1.png",
 };
 
 const zachsCave = {
@@ -36,32 +36,32 @@ const zachsCave = {
     {
       text:
         "> You get out of bed and go towards the curtain. As soon as you touch the glowing tear you’re sucked into a blinding white light.",
-      img: "",
+      img: "img/cave1.png",
     },
     {
       text:
         "> When the light fades, you’re in a strange room. A hooded figure appears before you.\n> He slowly lowers his hood. Is that…",
-      img: "",
+      img: "img/cave2.png",
     },
     {
       text:
         "> American actor, comedian, musician and writer Zach Galifinakis!? What’s he doing here?\n> Zach: Approach, seeker, and ask.\n> You: Where am I? How did I get here?",
-      img: "",
+      img: "img/cave3.png",
     },
     {
       text:
         "> Zach: Before I can answer that, you need to understand that the universe is a precarious collection of realities layered on top of each other and separated by bands of cosmic energy. Over time, the barriers between realities have become thin in some places. Thin enough to break…",
-      img: "",
+      img: "img/cave4.png",
     },
     {
       text:
         "> Zach: Something or someone in your world must have made a hole in the space time continuum. Now, you’re in the in-between. To return home and save your world from the rift, you’ll have to navigate through the dimensions that were affected by the breach and repair the tear.",
-      img: "",
+      img: "img/cave5.png",
     },
     {
       text:
         "> Zach: Look for signs of the rift to navigate between realms-- changes to the flow of space time have cascading effects. Make sure to stabilize the worlds you visit before you leave. But be careful playing God-- the choices you make will ripple. Choose a portal to start, and good luck!",
-      img: "",
+      img: "img/cave6.png",
     },
   ],
   question: "Choose a door.",
@@ -70,17 +70,17 @@ const zachsCave = {
       text: "Door 1",
       leadsTo: "jeffDavis",
       message: "Interesting choice...",
-      audio: "",
+      audio: "audio/laugh1.mp3",
     },
     {
       text: "Door 2",
       leadsTo: "wrbb",
       message: "Interesting choice...",
-      audio: "",
+      audio: "audio/laugh2.mp3",
     },
   ],
-  audio: "audio/example.mp3",
-  img: "img/example.png",
+  audio: "audio/cave.mp3",
+  img: "img/cave3.png",
 };
 
 const wrbb = {
@@ -88,7 +88,7 @@ const wrbb = {
     {
       text:
         ">  You blink, and the next think you know you’re standing in the WRBB studio. But it looks like someone missed their show, and the PSAs are on a loop!",
-      img: "",
+      img: "img/wrbb.png",
     },
   ],
   question: "Save the listeners with a song.",
@@ -119,7 +119,40 @@ const wrbb = {
       audio: "",
     },
   ],
-  audio: "",
+  audio: "audio/PSAs.mp3",
+  img: "",
+};
+
+const dmv = {
+  descriptions: [
+    {
+      text:
+        "> You’re standing in a large, drab room. The floors are linoleum and everything is just a little bit… ugly. \n> You: What is this place? It looks almost like… the DMV! \n > In front of you is a poster. You step forward to take a closer look.",
+      img: "img/dmv1.png",
+    },
+    {
+      text:
+        "> Something about this isn’t right… These rules of the road must have been messed up by the rift.", // use \n character for new lines
+      img: "img/DMV2.png",
+    },
+  ],
+  question: "What do you change?",
+  options: [
+    {
+      text: "> A... 2 and 4",
+      leadsTo: "pen15", // use null if no way forward
+      message:
+        "Okay, law abiding biker!",
+      audio: "",
+    },
+    {
+      text: "> B... These rules are better anyways. Thanks rift, no changes!",
+      leadsTo: "goodwill", // name of next scene object
+      message: "If you live long enough to bike again, you may regret this… ¯\_(ツ)_/¯",
+      audio: "",
+    },
+  ],
+  audio: "audio/mii.mp3",
   img: "",
 };
 
@@ -143,7 +176,7 @@ const jeffDavis = {
         "Burn it to the ground. He should pay for his crimes. Maybe the world would be better off without Jeff anyways… ",
       leadsTo: null,
       message: "Nice try, you petty bitch. Pick again.",
-      audio: "",
+      audio: "audio/r-u-kidding-me.mp3",
     },
     {
       text:
@@ -158,11 +191,11 @@ const jeffDavis = {
       leadsTo: "prideAndPrejudice",
       message:
         "Good call. It was just a TV show, and who would care about that?",
-      audio: "",
+      audio: "audio/r-u-kidding-me.mp3",
     },
   ],
-  audio: "",
-  img: "",
+  audio: "audio/city.mp3",
+  img: "img/jeff.png",
 };
 
 const beaconHills = {
@@ -170,22 +203,22 @@ const beaconHills = {
     {
       text:
         ">  You blink and you’re standing outside again, near a lacrosse field. It’s colder, and the moon is full. A howl pierces the air, and one of the players whips towards you, his red eyes piercing yours. Could this be… Beacon Hills?",
-      img: "",
+      img: "img/beaconhills1.png",
     },
     {
       text:
         "> You walk over to the sidelines. Everyone is here: Scott, Isaac, Lydia, Allison… Everyone except Stiles. He should be playing! And where is Derek?\n> Behind you, the pack of Alphas emerges from the woods. They head towards Scott-- looks like they’re going to attack!",
-      img: "",
+      img: "img/beaconhills2.png",
     },
     {
       text:
         "> You: It’ll be okay. With the help of Stiles and the rest of his pack, Scott will win.\n> Allison: Stiles? He’s barely been around since he and Derek started dating. He’s become a horrible friend, and without him and Derek here, Scott’s gonna die!",
-      img: "",
+      img: "img/beaconhills3.png",
     },
     {
       text:
         ">  Zach told you not to play God in other realms… maybe in order to save Scott you have to make things right.",
-      img: "",
+      img: "img/beaconhills1.png",
     },
   ],
   question: "How do you fix things in Beacon Hills?",
@@ -195,17 +228,131 @@ const beaconHills = {
         "I’m sorry… Save Scott? Pass. Focus instead on finding out more about how Sterek got together. Maybe someone has video?",
       leadsTo: null,
       message: "On the one hand, you save Sterek. On the other, you doom humanity if this change ripples across the universe in a calamitous way. Tough call there, but maybe reconsider.",
+      audio: "audio/r-u-kidding-me.mp3",
       audio: "",
     },
     {
       text:
         "Track down Stiles and Derek so they can come help Scott face the Alphas. Then, find a way to break them up (probably by seducing Stiles). It’s the only way.",
       leadsTo: "ilMondo",
-      message: "",
+      message: "Don’t cry because it’s over, smile because it happened.",
       audio: "",
     },
   ],
-  audio: "",
+  audio: "audio/teenwolf.mp3",
+  img: "img/beaconhills1.png",
+};
+
+const pen15 = {
+  descriptions: [
+    {
+      text:
+        "> You’re standing next to a middle school locker in a Limited Too cami and capris. Maya and Anna wait for you to put your books away.\n> The three of you overhear 7th grade heartthrob Brandt say to his friends: \“Yo, did you guys see the new UGIS list in the 2nd floor bathroom?\”", 
+      img: "img/pen151.png",
+    },
+    {
+      text:
+        ">  You charge up the stairs, throw open the door to the boy’s bathroom, and there it is: the official UGIS census. But wait a minute...something’s not quite right. ", // use \n character for new lines
+      img: "img/pen152.png",
+    },
+  ],
+  question: "Who isn’t UGIS?",
+  options: [
+    {
+      text: "> A... Vision",
+      leadsTo: null, // use null if no way forward
+      message:
+        "I’m sorry. Vision is UGIS. Try again.",
+      audio: "audio/agh.mp3",
+    },
+    {
+      text: "> B... Adam Driver",
+      leadsTo: null, // name of next scene object
+      message: "I’m sorry. Adam Driver is UGIS. Try again.",
+      audio: "audio/agh.mp3",
+    },
+    {
+      text: "> C... Jungkook",
+      leadsTo: "bike", // use null if no way forward
+      message:
+        "No one who would give up their own heart could ever be UGIS. #Junglin4ever",
+      audio: "audio/agh.mp3",
+    },
+    {
+      text: "> D... Catalina Berretta",
+      leadsTo: null, // use null if no way forward
+      message:
+        "I’m sorry. Catu is UGIS. If she didn’t want to be UGIS, she would always take out the compost on time. Try again.",
+      audio: "audio/r-u-kidding-me.mp3",
+    },
+  ],
+  audio: "audio/pen15.mp3",
+  img: "img/pen152.png",
+};
+
+const solidGround = {
+  descriptions: [
+    {
+      text:
+        ">  You find yourself standing in front of the Happiest Place on Earth. No, it’s not Disney World. It’s Solid Ground Cafe on Huntington Ave. \n> You enter and immediately crane your neck to try and see Lucia’s baby bump.",
+      img: "img/solidground1.png",
+    },
+    {
+      text:
+        ">  But Keith is at the counter! What the heck!", // use \n character for new lines
+      img: "img/solidground2.png",
+    },
+  ],
+  question: "What do you ask for?",
+  options: [
+    {
+      text: "> A… A large iced coffee with skim milk and extra caramel. ",
+      leadsTo: null, // use null if no way forward
+      message:
+        "Lucia overhears you and becomes suspicious-- she knows the real Catu would ask for a large iced coffee with skim milk, caramel and extra caramel. Somethings not right here, so she has Keith kick you out. Try again.",
+      audio: "audio/r-u-kidding-me.mp3",
+    },
+    {
+      text: "> B… Ask for Lucia instead. ",
+      leadsTo: "ryderHall", // name of next scene object
+      message: "Order is restored! Keith will scare away too many customers with his RBF. He should stick to what he does best: egg sandwiches.",
+      audio: "",
+    },
+  ],
+  audio: "audio/solidground.mp3",
+  img: "img/solidground1.png",
+};
+
+const stardew = {
+  descriptions: [
+    {
+      text:
+        ">  It’s the 24th of Spring. You stroll into town and wave hello to Mayor Lewis, Robin, and - oh look! Even Linus is enjoying the nice weather.\n> Today is the annual Flower Dance and you can’t wait to find yourself a dance partner. You enter the forest where the dance is taking place and...",
+      img: "img/stardew1.png",
+    },
+    {
+      text:
+        "> Mayor Lewis: Oh God, why isn’t anyone dancing?! Everyone is being so awkward...\n> It’s up to you to save the Flower Dance and make the first move.",
+      img: "img/stardew2.png",
+    },
+  ],
+  question: "Who do you ask to dance?",
+  options: [
+    {
+      text: "> A... Sebastian",
+      leadsTo: "nathanForYou", // use null if no way forward
+      message:
+        "Right on. He understands you on an emotional level and will help you with your coding homework.",
+      audio: "audio/not-compelte.mp3",
+    },
+    {
+      text: "> B... Haley",
+      leadsTo: "cabot", // name of next scene object
+      message: "Good call. There really can only be one emotional ocean in this relationship.",
+      audio: "audio/not-complete.mp3",
+    },
+  ],
+  audio: "audio/stardew.mp3",
   img: "",
 };
 
@@ -214,7 +361,13 @@ export const scenes = {
   catusRoom,
   zachsCave,
   wrbb,
+  dmv,
   jeffDavis,
   beaconHills,
+  pen15, 
+  goodwill, 
+  bike, 
+  solidGround, 
+  stardew
 };
 
